bf5f3469867ad25de298aab05b425a3b7eaade74
isKonstantin <me@knst.su>
2024-10-03 08:28:17 +0300

    Added missing allowed updates

--- src/main/java/app/finwave/tat/UpdatesProcessor.java
+++ src/main/java/app/finwave/tat/UpdatesProcessor.java
@@ -31,7 +31,13 @@ public class UpdatesProcessor implements UpdatesListener {
 
     protected Function<Update, Boolean> updateValidator;
 
-    protected GetUpdates updatesRequest = new GetUpdates().allowedUpdates("message", "message_reaction", "edited_channel_post", "callback_query");
+    protected GetUpdates updatesRequest = new GetUpdates().allowedUpdates(
+            "message", "edited_message", "message_reaction",
+            "channel_post", "edited_channel_post", "inline_query",
+            "chosen_inline_result", "callback_query", "shipping_query",
+            "pre_checkout_query", "poll", "poll_answer",
+            "chat_join_request"
+    );
 
     public void setAbstractGlobalHandler(AbstractGlobalHandler abstractGlobalHandler) {
         this.abstractGlobalHandler = abstractGlobalHandler;
